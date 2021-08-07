cfcf7a813633de7746dfeda314c08d06c0c0b3c8
Eric Tobin <eatobin@gmail.com>
2021-08-07 17:35:43 -0500

    cleanup

--- src/day2.c
+++ src/day2.c
@@ -5,8 +5,8 @@
 //#include "utils.h"
 
 int day2_1(const char prog[]) {
-    struct Intcode *ic = 0;
-    int ret = 0;
+    struct Intcode *ic;
+    int ret;
 
     if ((ic = intcode_new()) == NULL) {
         printf("Could not create intcode computer!\n");
@@ -27,7 +27,7 @@ int day2_1(const char prog[]) {
 int day2_2(const char prog[], int target_output) {
     int in1;
     int in2;
-    struct Intcode *ic = 0;
+    struct Intcode *ic;
 
     if ((ic = intcode_new()) == NULL) {
         printf("Could not create intcode computer!\n");
@@ -53,7 +53,7 @@ int day2_2(const char prog[], int target_output) {
 
 }
 
-int main(int argc, char *argv[]) {
+int main() {
     const char day2_intcode_program[] = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,2,9,19,23,2,13,23,27,1,6,27,31,2,6,31,35,2,13,35,39,1,39,10,43,2,43,13,47,1,9,47,51,1,51,13,55,1,55,13,59,2,59,13,63,1,63,6,67,2,6,67,71,1,5,71,75,2,6,75,79,1,5,79,83,2,83,6,87,1,5,87,91,1,6,91,95,2,95,6,99,1,5,99,103,1,6,103,107,1,107,2,111,1,111,5,0,99,2,14,0,0";
 
     printf("--- Day 2 ---\n");
